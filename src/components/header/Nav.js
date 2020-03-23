d0114e5740eba7c1f3b6e6e07ba4d0681c3666a8
TheFly555 <thijs@hejonet.nl>
2020-03-23 20:04:14 +0100

    start menu

--- src/components/header/Nav.js
+++ src/components/header/Nav.js
@@ -1,14 +1,34 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import "./nav.scss";
 
 class Nav extends Component {
-
-    render() {
-        return (
-            <nav>
-                <h1>Project B</h1>
-            </nav>
-        )
-    }
+  render() {
+    return (
+      <nav className='nav-main'>
+        <div className='logo'></div>
+        <div className='menu'>
+          <ul className='menu-items__list'>
+            <li className='menu-item'>
+              <a href='' className='menu-link'>
+                Home
+              </a>
+            </li>
+            <li className='menu-item'>
+              <a href='' className='menu-link'>
+                Films
+              </a>
+            </li>
+            <li className='menu-item'>
+              <a href='' className='menu-link'>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className='account'></div>
+      </nav>
+    );
+  }
 }
 
 export default Nav;
