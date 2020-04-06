c06394f64b028fdfa7689656921c18ef6b126a21
thijs <thijs>
2020-04-06 16:28:59 +0200

    added accoutn login in the header

--- app/src/components/header/Nav.js
+++ app/src/components/header/Nav.js
@@ -1,26 +1,35 @@
-import React, {Component} from "react";
-import {Link} from "react-router-dom";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Nav extends Component {
-
-    render() {
-        return (
-            <nav>
-                <section>
-                    <Link to={`/`}>Maak een account</Link>
-                    <Link to={`/`}>Login</Link>
-                </section>
-                <section>
-                    <h1>Project B</h1>
-                    <ul>
-                        <li><Link to={`/`}>Login</Link></li>
-                        <li><Link to={`/`}>Login</Link></li>
-                        <li><Link to={`/`}>Login</Link></li>
-                    </ul>
-                </section>
-            </nav>
-        )
-    }
+  render() {
+    return (
+      <nav className="region-nav">
+        <section className="account-login">
+          <Link className="create-account" to={`/`}>
+            Maak een account
+          </Link>
+          <Link to={`/`} classname="login">
+            Login
+          </Link>
+        </section>
+        <section className="navigation">
+          <h1>Project B</h1>
+          <ul className="navigation-links">
+            <li className="link">
+              <Link to={`/`}>Login</Link>
+            </li>
+            <li className="link">
+              <Link to={`/`}>Login</Link>
+            </li>
+            <li className="link">
+              <Link to={`/`}>Login</Link>
+            </li>
+          </ul>
+        </section>
+      </nav>
+    );
+  }
 }
 
 export default Nav;
