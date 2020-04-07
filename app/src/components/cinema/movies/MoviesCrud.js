5c9c63e658ad70015b84a792fefc584337ddb5c1
Stelios <stelios@vdmi.nl>
2020-04-07 23:16:10 +0200

    Added form

--- app/src/components/cinema/movies/MoviesCrud.js
+++ app/src/components/cinema/movies/MoviesCrud.js
@@ -15,34 +15,184 @@ class MoviesCrud extends Component{
         "keywords": ["Kaas", "Eng", "Hounted house"],
         "actors": ["Gordon", "Gerard"]
       }],
+      formProgress: 0.00,
+      valid: false,
+      inputs: {
+        title: '',
+        stars: '0',
+        description: '',
+        release_date: '',
+        genre: '',
+        studio: '',
+        keywords: '',
+        actors: '',
+      },
+      errors: {
+        title: '',
+        stars: '',
+        description: '',
+        release_date: '',
+        genre: '',
+        studio: '',
+        keywords: '',
+        actors: '',
+      },
     };
   }
 
-  sendData = () => {
+  handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    let {errors, inputs} = this.state;
 
+    switch (name) {
+      case 'title':
+        inputs.title = value;
+        errors.title =
+          value < 0
+            ? 'Vul de titel in'
+            : '';
+        break;
+      case 'stars':
+        inputs.stars = value;
+        errors.stars =
+          value < 0
+            ? 'Kies het aantal sterren'
+            : '';
+        break;
+      case 'description':
+        inputs.description = value;
+        errors.description =
+          value < 0
+            ? 'Vul de description aan'
+            : '';
+        break;
+      case 'release_date':
+        inputs.release_date = value;
+        errors.release_date =
+          value < 0
+            ? 'Vul een geldige datum in'
+            : '';
+        break;
+      case 'genre':
+        inputs.genre = value;
+        errors.genre =
+          value < 0
+            ? 'Vul de genre aan'
+            : '';
+        break;
+      case 'studio':
+        inputs.studio = value;
+        errors.studio =
+          value < 0
+            ? 'Vul de studio aan'
+            : '';
+        break;
+      case 'keywords':
+        inputs.keywords = value;
+        errors.keywords =
+          value < 0
+            ? 'Vul de keywords aan'
+            : '';
+        break;
+      case 'actors':
+        inputs.actors = value;
+        errors.actors =
+          value < 0
+            ? 'Vul de acteurs aan'
+            : '';
+    }
+
+    this.setState({errors, [name]: value}, ()=> {
+      console.log(errors)
+    });
+
+    this.state.valid = (this.validateForm(errors, inputs));
+    this.slider();
+  };
+
+  validateForm = (errors, inputs) => {
+    let valid = true;
+    Object.values(errors).forEach(
+      // if we have an error string set valid to false
+      (val) => val.length > 0 && (valid = false)
+    );
+
+    return valid;
+  };
+
+  slider = () => {
+    let {inputs} = this.state;
+    let inputsLength = Object.keys(inputs).length;
+    let filled = 0;
+
+    Object.values(inputs).forEach(
+      (val) => val.length > 0 && (filled += 1)
+    );
+
+    let progress = filled / inputsLength * 100;
+    this.setState({formProgress: progress});
   }
-
 }
 
 export class MoviesCreate extends MoviesCrud{
+
   render() {
+    let {formProgress, valid} = this.state;
     return(
       <main>
         <h2>Create a new movie</h2>
-        <progress value={1} max={100}/>
+        <progress value={formProgress} max={100}/>
         <form>
           <section>
             <p>
               <label>
                 Title:
               </label>
-              <input type={`text`} required/>
+              <input type={`text`} name={`title`} onChange={this.handleChange} noValidate/>
             </p>
             <p>
               <label>
-                Stars:
+                Description:
               </label>
-              <input type={`range`} min={0} max={10} required/>
+              <textarea name={`description`} onChange={this.handleChange} noValidate/>
+            </p>
+            <p>
+              <label>
+                Release date:
+              </label>
+              <input type={`date`} name={`release_date`} onChange={this.handleChange} noValidate/>
+            </p>
+          </section>
+          <section>
+            <p>
+              <label>
+                Genre:
+              </label>
+              <input type={`text`} name={`genre`} onChange={this.handleChange} noValidate/>
+            </p>
+            <p>
+              <label>
+                Studio:
+              </label>
+              <input type={`text`} name={`studio`} onChange={this.handleChange} noValidate/>
+            </p>
+            <p>
+              <label>
+                Actors:
+              </label>
+              <input type={`text`} name={`actors`} onChange={this.handleChange} noValidate/>
+            </p>
+          </section>
+          <section>
+            <p>
+              <label>
+                Keywords:
+              </label>
+              <input type={`text`} name={`keywords`} onChange={this.handleChange} noValidate/>
+            </p>
+            <p>
+              <input type={`submit`} name={`submit`} disabled={!valid} value={(valid) ? 'Verstuur' : 'Controleer de velden'}/>
             </p>
           </section>
         </form>
