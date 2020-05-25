a434de0149dda94c2e2aaf69b73a463ee37b851f
Givanni Schroevers <giovie2000@hotmail.com>
2020-05-25 14:07:46 +0200

    Add update and delete endpoints, update insomnia

--- system/cinema/views/movie.py
+++ system/cinema/views/movie.py
@@ -27,8 +27,6 @@ def movie_create(request):
 
     data = request.data
 
-    print(data)
-
     if 'genre' in data:
         genre = data['genre']
         genre, created = Genre.objects.get_or_create(name=genre)
@@ -87,7 +85,6 @@ def movie_create(request):
         file.save()
         data['photo_id'] = file.id
 
-    print(data)
     movie_validate = MovieCreateSerializer(data=data)
     movie_validate.is_valid(raise_exception=True)
     movie_validate.save()
@@ -95,9 +92,99 @@ def movie_create(request):
     return Response(movie_validate.data, status=status.HTTP_201_CREATED)
 
 
+@api_view(['PATCH'])
+def movie_update(request, pk):
+    if not request.data:
+        return Response({'detail': 'No data provided'}, status=status.HTTP_400_BAD_REQUEST)
+
+    data = request.data
+
+    try:
+        movie = Movie.objects.get(pk=pk)
+    except Movie.DoesNotExist:
+        return Response({'detail': f'Movie with id {pk} does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+    if 'genre' in data:
+        genre = data['genre']
+        genre, created = Genre.objects.get_or_create(name=genre)
+        if created:
+            genre.save()
+        movie.genre = genre.id
+
+    if 'keywords' in data:
+        keywords = data['keywords']
+
+        keyword_ids = []
+
+        for keyword in keywords:
+            keyword, created = Keyword.objects.get_or_create(word=keyword)
+            if created:
+                keyword.save()
+            keyword_ids.append(keyword.id)
+        data['keywords'] = keyword_ids
+
+    if 'actors' in data:
+        actors = data['actors']
+
+        actor_ids = []
+
+        for actor in actors:
+            actor, created = Actor.objects.get_or_create(name=actor)
+            if created:
+                actor.save()
+            actor_ids.append(actor.id)
+        data['actors'] = actor_ids
+
+    if 'photo' in data:
+        image_data = request.FILES['photo'].file
+
+        photo = ContentFile(image_data.read())
+        try:
+            latest_id = Movie.objects.latest("id").id
+        except Exception:
+            latest_id = 0
+        try:
+            image = Image.open(photo).convert('RGB')
+            image = ImageOps.fit(image, (260, 260), Image.ANTIALIAS)
+            photo_name = f'{settings.PHOTO_URL}{latest_id + 1}.jpg'
+            if not os.path.isdir(settings.PHOTO_ROOT):
+                os.mkdir(os.path.join(settings.MEDIA_ROOT, 'photos'))
+
+            image.save(f'{settings.MEDIA_ROOT}/{photo_name}', 'JPEG')
+        except IOError:
+            return Response({'detail': 'Photo is not a valid image'}, status=status.HTTP_400_BAD_REQUEST)
+
+        file = File()
+        file.path = photo_name
+        file.type = "image"
+        file.save()
+        movie.photo = file.id
+
+    movie_validate = MovieCreateSerializer(movie, data, partial=True)
+    movie_validate.is_valid(raise_exception=True)
+    movie_validate.save()
+
+    return Response(movie_validate.data, status=status.HTTP_200_OK)
+
+
+@api_view(['DELETE'])
+def movie_delete(request, pk):
+    try:
+        movie = Movie.objects.get(pk=pk)
+    except Movie.DoesNotExist:
+        return Response({'detail': f'Movie with id {pk} does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+    movie.delete()
+
+    return Response({'detail': f'Movie with id {pk} has been deleted'})
+
+
 @api_view(['GET'])
 def movie_detail(request, pk):
-    movie = Movie.objects.get(pk=pk)
+    try:
+        movie = Movie.objects.get(pk=pk)
+    except Movie.DoesNotExist:
+        return Response({'detail': f'Movie with id {pk} does not exist'}, status=status.HTTP_404_NOT_FOUND)
     data = MovieGetSerializer(movie).data
 
     return Response(data, status=status.HTTP_200_OK)
