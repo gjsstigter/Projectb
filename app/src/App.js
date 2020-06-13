f567916401fa35bfed3af2c745baaba238a8091e
TSM Mark Rutte <azhar_butt@hotmail.nl>
2020-06-13 16:36:01 +0200

    Register & Login toegevoegd

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,9 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./components/pages/Home";
+import FilmItem from "./components/pages/Film-item"
+import Login from "./components/pages/Login"
+import Register from "./components/pages/Register"
 import Header from "./components/header/Header";
 import P404 from "./components/pages/P404";
 import "./assets/sass/_main.scss";
@@ -15,6 +18,9 @@ class App extends Component {
           <Header />
           <Switch>
             <Route path="/" exact component={Home} />
+            <Route path="/film-item" exact component={FilmItem} />
+            <Route path="/login" exact component={Login} />
+            <Route path="/Register" exact component={Register} />
             <Route component={P404} />
           </Switch>
         </div>
