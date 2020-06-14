826a0c0a2a2d8a6fe9a0dc03777f913885e899fa
Stelios <stelios@vdmi.nl>
2020-06-14 14:03:00 +0200

    Add film item

--- app/src/App.js
+++ app/src/App.js
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-d
 import Home from "./components/pages/Home";
 import FilmItem from "./components/pages/Filmitem"
 import Header from "./components/header/Header";
-import Filmitem from "./components/pages/Filmitem";
 import Contact from "./components/pages/Contact";
 import './assets/sass/_main.scss'
 import Admin from "./components/pages/Admin";
@@ -24,10 +23,10 @@ class App extends Component {
                     <Header/>
                     <Switch>
                         <Route path={`/`} exact component={Home}/>
-                        <Route path={[`/admin/:page/:crud/:id`, `/admin/:page/:crud/`, `/admin/:page/`, `/admin/`, `/:page/:crud`]} component={Admin}/>
-                        <Route path={`/film/:id`} component={Filmitem}/>
+                        <Route path={`/films/:id`} component={FilmItem}/>
                         <Route path={`/films`} component={Films}/>
                         <Route path={`/contact`} component={Contact}/>
+                        <Route path={[`/admin/:page/:crud/:id`, `/admin/:page/:crud/`, `/admin/:page/`, `/admin/`, `/:page/:crud`]} component={Admin}/>
                         <Redirect path={`*`} to={`/`}/>
                     </Switch>
                 </div>
